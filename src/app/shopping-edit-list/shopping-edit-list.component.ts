c81aafb329ae5343dfaa70bf80670da5d9b8a854
pgaillot <p.gaillot@essteam.fr>
2022-02-14 15:12:31 +0100

    first code push

---
+++ src/app/shopping-edit-list/shopping-edit-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-shopping-edit-list",
+    templateUrl:"./shopping-edit-list.component.html"
+    
+})
+
+export class ShoppingEditListComponent{
+
+}
